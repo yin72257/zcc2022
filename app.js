147d3715c6df233bd6d526f551fffb80a8130e18
yin72257 <ruohangyin@gmail.com>
2021-11-27 01:07:07 +0800

    Add logic to ticket page

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
 const http = require("http");
 var express = require("express");
+var ticket = require("./routes/ticket");
+const port = 8000;
 var app = express();
 
 app.set('view engine', 'ejs');
 
-var ticket = require("./routes/ticket");
-
-const hostname = "127.0.0.1";
-const port = 8000;
-// Create HTTP server
-app.get('/', function(req, res) {
-    res.render("pages/ticket");
-});
-
-app.use('/tickets/', ticket);
-
+app.use('/', ticket);
   
 app.listen(port, function() {
   console.log(`Example app listening on port ${port}!`)
--- routes/ticket.js
+++ routes/ticket.js
@@ -2,42 +2,72 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const axios = require('axios');
+const url = require('url'); // built-in utility
 
-let raw_data = fs.readFileSync('./auth/auth.json');
-let auth = JSON.parse(raw_data);
-const username = auth.email;
-const password = auth.password;
-const zendesk = `https://${auth.subdomain}.zendesk.com/`
+const auth = JSON.parse(fs.readFileSync('./auth/auth.json'));
+const auth_input = {
+    username: auth.username,
+    password: auth.password
+};
+const zendesk = `https://${auth.subdomain}.zendesk.com`;
 // Home page route
-router.get('/', function (req, res) {
-    res.send('');
-});
-
-// About page route
-router.get('/:ticket_id', async function (req, res) {
-    const id = req.params.ticket_id;
-    const url = `${zendesk}/api/v2/tickets/${id}.json`;
+router.get('/', async function (req, res) {
+    let page_size = 25;
+    let list_url = `${zendesk}/api/v2/tickets.json?page[size]=`;
+    if (req.query.page && req.query.page.size) {
+        page_size = req.query.page.size;
+    } 
+    list_url += page_size;
+    if (req.query.page && req.query.page.after) {
+        list_url += `&page[after]=${req.query.page.after}`;
+    }
+    const count_url = `${zendesk}/api/v2/tickets/count.json`;
     try {
-        const result = await axios.get(url, {
-            auth: {
-                username: username,
-                password: password
-            }
+        const result = await axios.get(list_url, {
+            auth: auth_input
         });
-        const user_url = `${zendesk}/api/v2/users/${result.data.ticket.requester_id}.json`;
-        const requester = await axios.get(user_url, {
-            auth: {
-                username: username,
-                password: password
-            }
+        const count = await axios.get(count_url, {
+            auth: auth_input
         });
-        res.render('pages/ticket', {
-            requester: requester.data.user.name,
-            subject: result.data.ticket.raw_subject,
-            description: result.data.ticket.description
+        const next_query_params = result.data.links.next? result.data.links.next.replace(`${zendesk}/api/v2/tickets.json`,''): '';
+        const current_url = `${req.protocol}://${req.get('host')}`;
+        const next_url = `${current_url}${url.parse(req.url).pathname}${next_query_params}`;
+        const view_url = `${current_url}/view/`;
+        const back_url = `${current_url}${url.parse(req.url).pathname}`;
+        res.render('pages/list', {
+            tickets: result.data.tickets,
+            total: count.data.count.value,
+            current: result.data.tickets.length,
+            has_more: result.data.meta.has_more,
+            next_url: next_url,
+            view_url: view_url,
+            back_url: back_url
         });
     } catch (err) {
         console.log(err);
+    }
+});
+
+router.get('/view/:ticket_id', async function (req, res) {
+    const id = req.params.ticket_id;
+    const ticket_url = `${zendesk}/api/v2/tickets/${id}.json`;
+    try {
+        const result = await axios.get(ticket_url, {
+            auth: auth_input
+        });
+        const user_url = `${zendesk}/api/v2/users/${result.data.ticket.requester_id}.json`;
+        const requester = await axios.get(user_url, {
+            auth: auth_input
+        });
+        const back_url = `${req.protocol}://${req.get('host')}`;
+        res.render('pages/ticket', {
+            requester: requester.data.user.name,
+            subject: result.data.ticket.raw_subject,
+            description: result.data.ticket.description,
+            back_url: back_url
+        });
+    } catch (err) {
+        console.log(id);
     }
 });
 
