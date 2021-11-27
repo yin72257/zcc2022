6bcd7e0c82c72009f0a426488ed98d1452002baf
yin72257 <ruohangyin@gmail.com>
2021-11-27 17:25:32 +0800

    Add middleware for route to detect whether auth is valid

--- routes/ticket.js
+++ routes/ticket.js
@@ -4,17 +4,38 @@ const axios = require('axios');
 const url = require('url'); // built-in utility
 const { list_error, ticket_error } = require('../error_handling/ticket_error');
 
-const auth = JSON.parse(fs.readFileSync('./auth/auth.json'));
-const auth_input = {
-    username: auth.username,
-    password: auth.password
-};
 const router = express.Router();
-const zendesk = `https://${auth.subdomain}.zendesk.com`;
+
+router.use(function (req, res, next) {
+    try {
+        const auth = JSON.parse(fs.readFileSync('./auth/auth.json')); 
+        if (!auth.username) {
+            res.status(401).send('Username not found in auth.json');
+            return;
+        }
+        if (!auth.password) {
+            res.status(401).send('Password not found in auth.json');
+            return;
+        }
+        if (!auth.subdomain) {
+            res.status(401).send('Subdomain not found in auth.json');
+            return;
+        }
+        res.locals.auth_input = {
+            username: auth.username,
+            password: auth.password
+        };
+        res.locals.zendesk = `https://${auth.subdomain}.zendesk.com`;
+        next()
+    } catch (err) {
+        res.status(401).send('auth/auth.json is empty or not found');
+    }
+
+})
 
 async function list_handle(req, res) {
     let page_size = 25;
-    let list_url = `${zendesk}/api/v2/tickets.json?page[size]=`;
+    let list_url = `${res.locals.zendesk}/api/v2/tickets.json?page[size]=`;
     if (req.query.page && req.query.page.size) {
         page_size = req.query.page.size;
     }
@@ -22,15 +43,15 @@ async function list_handle(req, res) {
     if (req.query.page && req.query.page.after) {
         list_url += `&page[after]=${req.query.page.after}`;
     }
-    const count_url = `${zendesk}/api/v2/tickets/count.json`;
+    const count_url = `${res.locals.zendesk}/api/v2/tickets/count.json`;
     try {
         const result = await axios.get(list_url, {
-            auth: auth_input
+            auth: res.locals.auth_input
         });
         const count = await axios.get(count_url, {
-            auth: auth_input
+            auth: res.locals.auth_input
         });
-        const links = generate_links(req, result.data.links);
+        const links = generate_links(req, result.data.links, res.locals.zendesk);
         res.render('pages/list', {
             tickets: result.data.tickets,
             total: count.data.count.value,
@@ -49,7 +70,7 @@ async function list_handle(req, res) {
     }
 }
 
-function generate_links(req, links) {
+function generate_links(req, links, zendesk) {
     const next_query_params = links.next ? links.next.replace(`${zendesk}/api/v2/tickets.json`, '') : '';
     const current_url = `${req.protocol}://${req.get('host')}`;
     const next_url = `${current_url}${url.parse(req.url).pathname}${next_query_params}`;
@@ -64,14 +85,14 @@ function generate_links(req, links) {
 
 async function view_ticket_handle(req, res) {
     const id = req.params.ticket_id;
-    const ticket_url = `${zendesk}/api/v2/tickets/${id}.json`;
+    const ticket_url = `${res.locals.zendesk}/api/v2/tickets/${id}.json`;
     try {
         const result = await axios.get(ticket_url, {
-            auth: auth_input
+            auth: res.locals.auth_input
         });
-        const user_url = `${zendesk}/api/v2/users/${result.data.ticket.requester_id}.json`;
+        const user_url = `${res.locals.zendesk}/api/v2/users/${result.data.ticket.requester_id}.json`;
         const requester = await axios.get(user_url, {
-            auth: auth_input
+            auth: res.locals.auth_input
         });
         const back_url = `${req.protocol}://${req.get('host')}`;
         res.render('pages/ticket', {
--- tests/routes/ticket.test.js
+++ tests/routes/ticket.test.js
@@ -1,22 +1,22 @@
 const ticket = require('../../routes/ticket');
 const fs = require('fs');
 const { expect } = require('@jest/globals');
-const auth = JSON.parse(fs.readFileSync('./auth/auth.json'));
-const zendesk = `https://${auth.subdomain}.zendesk.com`;
+
 
 
 describe('Test the generate links methods', () => {
     test('It should response the GET method', async () => {
+        const zendesk = `https://subdomain.zendesk.com`;
         const req = {
             protocol: 'http',
             url: '/?page[size]=25&page[after]=A',
-            get: (param) => param == 'host'? 'localhost:8000': ''
+            get: (param) => param == 'host' ? 'localhost:8000' : ''
         };
         const query_params = '?page[size]=25&page[after]=A';
         const links = {
             next: `${zendesk}/api/v2/tickets.json${query_params}`
         }
-        const response = ticket.generate_links(req, links);
+        const response = ticket.generate_links(req, links, zendesk);
         expect(response.next_url).toBe('http://localhost:8000/?page[size]=25&page[after]=A');
         expect(response.view_url).toBe('http://localhost:8000/view/');
         expect(response.back_url).toBe('http://localhost:8000/');
